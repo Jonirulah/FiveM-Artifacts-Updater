38d986d181570fc83f860d98e2409b1776e157ea
Jonirulah <Jonathanjoni12@hotmail.com>
2023-07-22 01:09:19 +0200

    v 1.0.1
    Unattended mode added

--- updateArtifacts.py
+++ updateArtifacts.py
@@ -12,9 +12,11 @@ from tabulate import tabulate
 import time
 import subprocess
 import shutil
+import sys
 
 FIVEM_DIRECTORY = "/fivem/myserver"
 CACHE_DIRECTORY = "/fivem/myserver/server-data/cache"
+UNATTENDED = False
 
 class bcolors:
     HEADER = '\033[95m'
@@ -27,7 +29,7 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-def download_and_extract(url, destination_dir):
+def download_and_extract(url, destination_dir, select_cache):
     # Download the fx.tar.xz file
     print(bcolors.ENDC + "Downloading: ", url)
     response = requests.get(url, stream=True)
@@ -49,13 +51,13 @@ def download_and_extract(url, destination_dir):
             progress = (downloaded_size / total_size) * 100
             print(bcolors.ENDC + f"Download progress: {progress:.2f}% - {downloaded_size / 1024 / 1024}/{total_size / 1024 / 1024} MB", end='\r')
 
-
-    print(bcolors.BOLD + "# WARNING!")
-    print(bcolors.WARNING + "This process will stop the FiveM service if your server is running a rollback and data loss will occur!")
-    time.sleep(1)
-    cont = str(input(bcolors.WARNING + "Do you want to continue? (y/n or CTRL+C to exit forcefully): ")).lower()
-    if cont != "y":
-        os._exit(0)
+    if not UNATTENDED:
+        print(bcolors.BOLD + "# WARNING!")
+        print(bcolors.WARNING + "This process will stop the FiveM service if your server is running a rollback and data loss will occur!")
+        time.sleep(1)
+        cont = str(input(bcolors.WARNING + "Do you want to continue? (y/n or CTRL+C to exit forcefully): ")).lower()
+        if cont != "y":
+            os._exit(0)
 
     # Stop FiveM Service
     try:
@@ -92,55 +94,119 @@ def delete_folder(folder):
 def delete_cache(cache, folder):
     # Delete Cache of the server
     if cache == "y":
+        print(bcolors.OKGREEN + "Clearing Cache!")
         delete_folder(folder)
     else:
         return
+    
+def check_unattended_mode():
+    global UNATTENDED
+    # Run the script automatically, unattended.
+    args = sys.argv
+    select_cache = "n"
+    build_arg = "latest"
+    if len(args) > 2:
+        print("Script is running in unattended mode.")
+        UNATTENDED = True
+        # Iterate over the command-line arguments
+        i = 1
+        while i < len(sys.argv):
+            arg = sys.argv[i]
+            if arg == "-b" or arg == "--build":
+                if i + 1 < len(sys.argv):
+                    build_arg = sys.argv[i + 1]
+                    if build_arg != "latest":
+                        build_arg = int(build_arg)  # Convert the argument to an integer or the appropriate data type
+                        i += 1
+                else:
+                    print("Missing value for -b option.")
+                    return
 
-req = requests.get("https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/", timeout=5)
-soup = BeautifulSoup(req.text, 'lxml')
+            elif arg == "-c" or arg == "--clear-cache":
+                select_cache = "y"
+            i += 1
+        return build_arg, select_cache
+    return None, None
 
+def download_latest(select_cache):
+    # Scrap and Download Last Build
+    last_build = soup.find('div', class_='level-left').text
+    last_build_version = re.findall(r"[0-9].*", last_build)[0]
+    url = soup.find('a', class_='is-active')['href'][1:]
+    last_build_url = "https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master" + url
+    print(bcolors.OKGREEN + "Build has been found!", last_build_version)
+    download_and_extract(last_build_url, FIVEM_DIRECTORY, select_cache)
 
-select_build = str(input("Do you wanna select a build? (y/n): ")).lower()
-select_cache = str(input("Do you wanna delete the FiveM server cache? (y/n): ")).lower()
-
-if (select_build) == "y":
-    # Download Custom Build
+def download_custom(selected_build, select_cache):
+    # Download Custom from 
     link = soup.find_all('a', class_='panel-block')
     builds = soup.find_all('div', class_='level-left')
-    dates = soup.find_all('div', class_='level-item')
-    print(bcolors.ENDC + "Last builds: (order by date) [INFO: You can choose any other not listed here.]")
-    data = []
-
-    time.sleep(1)
-
-    # Just for pretty printing, show information to the administrator regarding last updates.
-    for build in range(15): # You can change it to whatever you want.
-        build_version = re.findall(r"[0-9].*", builds[build].text)[0]
-        date = dates[build].text
-        data.append([build_version, date, link[build+1]['href'][1:]])
-    print(tabulate(data, headers=['Build', 'Release Date', 'URL'], tablefmt='fancy_grid', colalign=("center","center","center")))
-
-    time.sleep(1)
-
-    # Choose a build number, and look for it. (search method could be improved ikr)
-    selected_build = int(input("\nEnter build number: "))
     found = False
-
     for build in range(len(builds)):
         build_version = re.findall(r"[0-9].*", builds[build].text)[0]
         if int(build_version) == int(selected_build):
             print(bcolors.OKGREEN + "Build has been found!", link[build+1]['href'][1:])
             build_url = "https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master" + link[build+1]['href'][1:]
             found = True
-            download_and_extract(build_url, FIVEM_DIRECTORY)
+            download_and_extract(build_url, FIVEM_DIRECTORY, select_cache)
             break
     if not found:
         print(bcolors.FAIL + "Build has not been found!")
+
+# Attended routine
+def not_unattended():
+    # Prompt for details
+    select_build = str(input("Do you wanna select a build? (y/n): ")).lower()
+    select_cache = str(input("Do you wanna delete the FiveM server cache? (y/n): ")).lower()
+    # We want a custom build
+    if (select_build) == "y":
+        # Download Custom Build
+        link = soup.find_all('a', class_='panel-block')
+        builds = soup.find_all('div', class_='level-left')
+        dates = soup.find_all('div', class_='level-item')
+        print(bcolors.ENDC + "Last builds: (order by date) [INFO: You can choose any other not listed here.]")
+        data = []
+
+        time.sleep(1)
+
+        # Just for pretty printing, show information to the administrator regarding last updates.
+        for build in range(15): # You can change it to whatever you want.
+            build_version = re.findall(r"[0-9].*", builds[build].text)[0]
+            date = dates[build].text
+            data.append([build_version, date, link[build+1]['href'][1:]])
+        print(tabulate(data, headers=['Build', 'Release Date', 'URL'], tablefmt='fancy_grid', colalign=("center","center","center")))
+
+        time.sleep(1)
+
+        # Choose a build number, and look for it. (search method could be improved ikr)
+        if not UNATTENDED:
+            selected_build = int(input("\nEnter build number: "))
+
+        found = False
+        download_custom(selected_build, select_cache)
+    # We don't want a custom build
+    else:
+        download_latest(select_cache)
+
+# Unattended Routine
+def unattended(selected_build, select_cache):
+    if selected_build != "latest":
+        download_custom(selected_build, select_cache)
+    else:
+        download_latest(select_cache)
+
+# MAIN ROUTINE
+
+# Check if running unattended and fetch cmd args.
+selected_build, select_cache = check_unattended_mode()
+
+req = requests.get("https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/", timeout=5)
+soup = BeautifulSoup(req.text, 'lxml')
+
+# If not running Unattended Mode.
+if not UNATTENDED:
+    not_unattended()
 else:
-    # Download Last Build
-    last_build = soup.find('div', class_='level-left').text
-    last_build_version = re.findall(r"[0-9].*", last_build)[0]
-    url = soup.find('a', class_='is-active')['href'][1:]
-    last_build_url = "https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master" + url
-    print(bcolors.OKGREEN + "Build has been found!", last_build_version)
-    download_and_extract(last_build_url, FIVEM_DIRECTORY)
+    unattended(selected_build,select_cache)
+
+    
